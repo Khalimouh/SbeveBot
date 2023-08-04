fa29f1b40a5ce9c3c50b2df8e55e75a4036246be
Mohamed Ramdane DEBIANE <M.debiane71@gmail.com>
2023-08-05 00:31:01 +0200

    WIP: music player

--- sbevebot/steve.py
+++ sbevebot/steve.py
@@ -3,7 +3,9 @@ import os
 import sys
 import logging
 import random
+import json
 
+from yt_dlp import YoutubeDL
 from dotenv import load_dotenv
 from pathlib import Path
 from discord import Intents
@@ -31,6 +33,16 @@ class SteveBot(discord.Client):
     def __init__(self, *, intents: Intents, **options: Any) -> None:
         super().__init__(intents=intents, **options)
         self.emoji_list = ['🍆', '🤏']
+        self.voice_channel = None
+        self.ytdl_options = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        self.ffmpeg_options = {'options': '-vn'}
 
     async def on_ready(self):
         for _ in self.guilds:
@@ -47,6 +59,49 @@ class SteveBot(discord.Client):
                 await msg.add_reaction(self.emoji_list[1])
             else:
                 await msg.add_reaction(self.emoji_list[0])
+        if message.content.startswith("!join"):
+            if message.author.voice and message.author.voice.channel:
+                channel = message.author.voice.channel
+                self.voice_channel = await channel.connect()
+            else:
+                await message.reply("You are not connected to a voice channel, retard !")
+        if message.content.startswith("!leave"):
+            if self.voice_channel:
+                await self.voice_channel.disconnect()
+                self.voice_channel = None
+            else:
+                await message.reply("You are not connected to a voice channel, retard !")
+        if message.content.startswith("!play"):
+
+            if message.author.voice and message.author.voice.channel:
+                channel = message.author.voice.channel
+                self.voice_channel = await channel.connect()
+            else:
+                await message.reply("You are not connected to a voice channel, retard !")
+            # On parse la commande
+            tokens = message.content.split(" ")
+
+            if len(tokens) != 2:
+                return await message.reply("Usage: !play [url]")
+
+            input_url = tokens[1]
+
+            print(message.content)
+            print(input_url, message.author)
+
+            with YoutubeDL(self.ytdl_options) as yt:
+                info = yt.extract_info(input_url, download=False)
+                # print(json.dumps(yt.sanitize_info(info)))
+            if info:
+                try:
+                    print(self.voice_channel)
+                    self.voice_channel.play(discord.FFmpegOpusAudio(info["url"]))
+                    self.voice_channel.source = discord.PCMVolumeTransformer(self.voice_channel.source)
+                    self.voice_channel.source.volume = 0.7
+                except TypeError as e:
+                    print(e)
+            else:
+                return
 
 
 if __name__ == "__main__":
